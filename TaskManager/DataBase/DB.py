5739888a9aad9acbdcdbddcf62df8f558afc5f21
Python <py_official@mail.ru>
2023-11-20 23:31:08 +0300

    test

--- TaskManager/DataBase/DB.py
+++ TaskManager/DataBase/DB.py
@@ -1,17 +1,22 @@
 import sqlite3 as sq
-import calendar
-month = {1: 'January',
-         2: 'February',
-         3: 'March',
-         4: 'April',
-         5: 'May',
-         6: 'June',
-         7: 'July',
-         8: 'August',
-         9: 'September',
-         10: 'October',
-         11: 'November',
-         12: 'December'}
+from calendar import monthcalendar, weekday
+from time import time
+
+# init helpful vars
+month = {
+    1: 'January',
+    2: 'February',
+    3: 'March',
+    4: 'April',
+    5: 'May',
+    6: 'June',
+    7: 'July',
+    8: 'August',
+    9: 'September',
+    10: 'October',
+    11: 'November',
+    12: 'December'
+}
 
 day = {
     0: 'Monday',
@@ -21,29 +26,40 @@ day = {
     4: 'Friday'
 }
 
+
 #алгоритм расчёта рабочитх дней
 def count_working_days(year, month):
-    cal = calendar.monthcalendar(year, month)
+    # getting calendar
+    cal = monthcalendar(year, month)
+    # create counter for work days
     working_days_count = 0
 
+    # run cycle week in month
     for week in cal:
+        # run cycle day in week
         for day in week:
             # Добавляем только дни текущего месяца
             if day != 0:
                 # Добавляем только рабочие дни (пн-пт)
-                if calendar.weekday(year, month, day) < 5:
+                if weekday(year, month, day) < 5:
                     working_days_count += 1
 
     return working_days_count
-#создание базы данных
+
+
+# создание базы данных
 def create_database(year):
+    # connect to db
     connect = sq.connect(f'{year}.db')
     create_month_table(connect)
     create_days_table(connect, year)
+    # close connect
     connect.close()
 
-#создание таблицы месяцев
+
+# создание таблицы месяцев
 def create_month_table(connect):
+
     for i in range (1,13):
         quary_create_table= f'''
         CREATE TABLE IF NOT EXISTS {month[i]} (
@@ -58,9 +74,10 @@ def create_month_table(connect):
         '''
         connect.cursor().execute(quary_insert, (month[i],))
         connect.commit()
-#создание и заполнение таблицы дней
-def create_days_table (connect, year):
 
+
+# создание и заполнение таблицы дней
+def create_days_table (connect, year):
     for i in range(1,13):
         quary_create_table = f'''
         CREATE TABLE IF NOT EXISTS DaysOfWeek{month[i]}(
@@ -71,30 +88,22 @@ def create_days_table (connect, year):
         )
         '''
         connect.cursor().execute(quary_create_table)
-        connect.commit()
-        count = 0#получение первого рабочего дня месяца
+        count = 0
+        #получение первого рабочего дня месяца
         #заполнение дней в таблице дней
-        for _ in range(1, count_working_days(year, i) + 1):
+        for _ in range(count_working_days(year, i)):
 
             query_insert = f'''
             INSERT INTO DaysofWeek{month[i]}(month_id, name)
             VALUES (?,?)
            '''
             connect.cursor().execute(query_insert, (i,day[count],))
-
-            if count == 4:
-                count = 0
-            else:
-                count += 1
+            count = count + 1 if count < 4 else 0
         connect.commit()
 
 
-
-
-create_database(2023)
-
-
-
-
-
-
+if __name__ == "__main__":
+    start = time()
+    create_database(2023)
+    end = time()
+    print(f"\n\n[*] Speed test ILO\nFunc > create_database()\nArgs > 2023: int\nResult > {end-start}")
