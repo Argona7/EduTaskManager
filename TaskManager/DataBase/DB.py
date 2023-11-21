b1b257ab0f8e6df2acec8d084ac4162d4e706a46
Argona <takemyskills03@gmail.com>
2023-11-21 21:07:34 +0300

    Commit

--- TaskManager/DataBase/DB.py
+++ TaskManager/DataBase/DB.py
@@ -1,6 +1,6 @@
 import sqlite3 as sq
-from calendar import monthcalendar, weekday, monthrange
-
+from calendar import monthcalendar,monthrange
+from time import time
 
 # init helpful vars
 month = {
@@ -31,6 +31,7 @@ day = {
 def count_working_days(year, month):
     # getting calendar
     cal = monthcalendar(year, month)
+    first_day, _ = monthrange(year, month)
     # create counter for work days
     working_days_count = 0
     days_count = []
@@ -40,11 +41,10 @@ def count_working_days(year, month):
         # run cycle day in week
         for day in week:
             # Добавляем только дни текущего месяца
-            if day != 0:
-                # Добавляем только рабочие дни (пн-пт)
-                if weekday(year, month, day) < 5:
-                    working_days_count += 1
-                    days_count.append(day)
+            # Добавляем только рабочие дни (пн-пт)
+            if day != 0 and (first_day + day - 1) % 7 < 5:
+                working_days_count += 1
+                days_count.append(day)
 
     return (working_days_count, days_count)
 
@@ -52,11 +52,12 @@ def count_working_days(year, month):
 # создание базы данных
 def create_database(year):
     # connect to db
-    connect = sq.connect(f'{year}.db')
-    create_month_table(connect)
-    create_days_table(connect, year)
+    db_name  = f'{year}.db'
+    with sq.connect(db_name) as connect:
+        create_month_table(connect)
+        create_days_table(connect, year)
     # close connect
-    connect.close()
+
 
 
 # создание таблицы месяцев
@@ -75,7 +76,7 @@ def create_month_table(connect):
         VALUES (?)
         '''
         connect.cursor().execute(quary_insert, (month[i],))
-        connect.commit()
+    connect.commit()
 
 
 # создание и заполнение таблицы дней
@@ -91,10 +92,7 @@ def create_days_table (connect, year):
         )
         '''
         connect.cursor().execute(quary_create_table)
-        count = monthrange(year,i)[0] #получение первого рабочего дня месяца
-        if count == 5 or count == 6:
-            count = 0
-
+        count = monthrange(year,i)[0] if monthrange(year,i)[0] < 5 else 0 #получение первого рабочего дня месяца
         len, date = count_working_days(year, i)
         #заполнение дней в таблице дней
         for j in range(len):
@@ -105,10 +103,16 @@ def create_days_table (connect, year):
            '''
             connect.cursor().execute(query_insert, (i,date[j],day[count],))
             count = count + 1 if count < 4 else 0
-        connect.commit()
+    connect.commit()
+
+
+
 
 
 
 
 if __name__ == "__main__":
+    start = time()
     create_database(2023)
+    end = time()
+    print(end-start)
