91c15e64d23982b719642b0f8cb54aa4bd56282d
Argona7 <146877884+Argona7@users.noreply.github.com>
2024-02-27 22:44:34 +0300

    Changing database, server
    Changing the database architecture and changing the data output on the server

--- TaskManager/scripts/DB.py
+++ TaskManager/scripts/DB.py
@@ -3,55 +3,55 @@ from calendar import monthcalendar, monthrange
 from argparse import ArgumentParser
 
 # init helpful vars
-month = {
-    1: 'January',
-    2: 'February',
-    3: 'March',
-    4: 'April',
-    5: 'May',
-    6: 'June',
-    7: 'July',
-    8: 'August',
-    9: 'September',
-    10: 'October',
-    11: 'November',
-    12: 'December'
-}
-
-day = {
-    0: 'Понедельник',
-    1: 'Вторник',
-    2: 'Среда',
-    3: 'Четверг',
-    4: 'Пятница'
-}
+# month = {
+#     1: 'January',
+#     2: 'February',
+#     3: 'March',
+#     4: 'April',
+#     5: 'May',
+#     6: 'June',
+#     7: 'July',
+#     8: 'August',
+#     9: 'September',
+#     10: 'October',
+#     11: 'November',
+#     12: 'December'
+# }
+#
+# day = {
+#     0: 'Понедельник',
+#     1: 'Вторник',
+#     2: 'Среда',
+#     3: 'Четверг',
+#     4: 'Пятница'
+# }
 
 
 # алгоритм расчёта рабочитх дней
-def count_working_days(year, month):
-    # getting calendar
-    cal = monthcalendar(year, month)
-    first_day, _ = monthrange(year, month)
-    # create counter for work days
-    working_days_count = 0
-    days_count = []
-
-    # run cycle week in month
-    for week in cal:
-        # run cycle day in week
-        for day in week:
-            # Добавляем только дни текущего месяца
-            # Добавляем только рабочие дни (пн-пт)
-            if day != 0 and (first_day + day - 1) % 7 < 5:
-                working_days_count += 1
-                days_count.append(day)
-
-    return (working_days_count, days_count)
+# def count_working_days(year, month):
+#     # getting calendar
+#     cal = monthcalendar(year, month)
+#     first_day, _ = monthrange(year, month)
+#     # create counter for work days
+#     working_days_count = 0
+#     days_count = []
+#
+#     # run cycle week in month
+#     for week in cal:
+#         # run cycle day in week
+#         for day in week:
+#             # Добавляем только дни текущего месяца
+#             # Добавляем только рабочие дни (пн-пт)
+#             if day != 0 and (first_day + day - 1) % 7 < 5:
+#                 working_days_count += 1
+#                 days_count.append(day)
+#
+#     return (working_days_count, days_count)
 
 
 def main():
     parser = ArgumentParser(description="Создание базы данных для указаннного года ")
-    parser.add_argument('year', type=int, help='Год для создании ьазы данных')
+    parser.add_argument('year', type=int, help='Год для создании базы данных')
     args = parser.parse_args()
 
     create_database(args.year)
@@ -62,61 +62,60 @@ def create_database(year):
     # connct to db
     db_name = f'{year}.db'
     with sq.connect(db_name) as connect:
-        create_month_table(connect)
-        create_days_table(connect, year)
+        # create_month_table(connect)
+        # create_days_table(connect, year)
         create_teachers_table(connect)
         create_classes_table(connect)
         create_lessons_table(connect)
+        create_schedule_table(connect, ['2024','03','26'])
         return connect
     # close connect
 
 
 # создание таблицы месяцев
-def create_month_table(connect):
-    quary_create_table = f'''
-    CREATE TABLE IF NOT EXISTS Months (
-    id INTEGER PRIMARY KEY,
-    month_name TEXT NOT NULL
-    ) 
-    '''
-    connect.cursor().execute(quary_create_table)
-    values = [(month[i],) for i in
-              range(1, 13)]  # массив values для заполнения таблицы Months поля month_name с помощью словаря month
-    quary_insert = f'''
-    INSERT INTO Months (month_name)
-    VALUES (?)
-    '''
-    connect.cursor().executemany(quary_insert, values)
-    connect.commit()
+# def create_month_table(connect):
+#     quary_create_table = f'''
+#     CREATE TABLE IF NOT EXISTS Months (
+#     id INTEGER PRIMARY KEY,
+#     month_name TEXT NOT NULL
+#     )
+#     '''
+#     connect.cursor().execute(quary_create_table)
+#     values = [(month[i],) for i in
+#               range(1, 13)]  # массив values для заполнения таблицы Months поля month_name с помощью словаря month
+#     quary_insert = f'''
+#     INSERT INTO Months (month_name)
+#     VALUES (?)
+#     '''
+#     connect.cursor().executemany(quary_insert, values)
+#     connect.commit()
 
 
 # создание и заполнение таблицы дней
-def create_days_table(connect, year):
-    for i in range(1, 13):
-        quary_create_table = f'''
-        CREATE TABLE IF NOT EXISTS DaysOfWeek{month[i]}(
-        id INTEGER PRIMARY KEY,
-        month_id INTEGER NOT NULL,
-        date TEXT NOT NULL,
-        name TEXT NOT NULL,
-        FOREIGN KEY (month_id) REFERENCES Months(id) ON UPDATE CASCADE ON DELETE CASCADE
-        )
-        '''
-        connect.cursor().execute(quary_create_table)
-        count = monthrange(year, i)[0] if monthrange(year, i)[0] < 5 else 0  # получение первого рабочего дня месяца
-        len, date = count_working_days(year, i)
-        count_arr = [count]  # массив дней недели для execute_many
-        # заполнение дней в таблице дней
-        for j in range(len):
-            count = count + 1 if count < 4 else 0
-            count_arr.append(count)
-        values = [(i, date[j], day[count_arr[j]]) for j in range(len)]  # массив со всеми значениями
-        query_insert = f'''
-         INSERT INTO DaysofWeek{month[i]}(month_id, date,name)
-         VALUES (?,?,?)
-        '''
-        connect.cursor().executemany(query_insert, values)
-    connect.commit()
+# def create_days_table(connect, year):
+#     for i in range(1, 13):
+#         quary_create_table = f'''
+#         CREATE TABLE IF NOT EXISTS DaysOfWeek{month[i]}(
+#         id INTEGER PRIMARY KEY,
+#         date TEXT NOT NULL,
+#         name TEXT NOT NULL,
+#         )
+#         '''
+#         connect.cursor().execute(quary_create_table)
+#         count = monthrange(year, i)[0] if monthrange(year, i)[0] < 5 else 0  # получение первого рабочего дня месяца
+#         len, date = count_working_days(year, i)
+#         count_arr = [count]  # массив дней недели для execute_many
+#         # заполнение дней в таблице дней
+#         for j in range(len):
+#             count = count + 1 if count < 4 else 0
+#             count_arr.append(count)
+#         values = [(i, date[j], day[count_arr[j]]) for j in range(len)]  # массив со всеми значениями
+#         query_insert = f'''
+#          INSERT INTO DaysofWeek{month[i]}(month_id, date,name)
+#          VALUES (?,?,?)
+#         '''
+#         connect.cursor().executemany(query_insert, values)
+#     connect.commit()
 
 
 def create_teachers_table(connect):
@@ -144,7 +143,7 @@ def create_lessons_table(connect):
     CREATE TABLE IF NOT EXISTS Lessons (
     id INTEGER PRIMARY KEY,
     name TEXT NOT NULL UNIQUE,
-    teacher_id TEXT NOT NULL,
+    teacher_id INTEGER NOT NULL,
     FOREIGN KEY (teacher_id)  REFERENCES Teachers (id) ON UPDATE CASCADE ON DELETE CASCADE
     )
     '''
@@ -156,10 +155,10 @@ def create_schedule_table(connect, date: list):
     query_create_table = f'''
     CREATE TABLE IF NOT EXISTS Schedule_{date[0]}_{date[1]}_{date[2]}(
     id INTEGER PRIMARY KEY,
-    day_id INTEGER,
+    day INTEGER,
     lesson_id INTEGER,
-    class_id INTEGER,
-    FOREIGN KEY (day_id) REFERENCES DaysOfWeek{month[int(date[1])]}(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    class_id INTEGER ,
+    lesson_time TEXT NOT NULL,
     FOREIGN KEY (lesson_id) REFERENCES Lessons (id) ON UPDATE CASCADE ON DELETE CASCADE,
     FOREIGN KEY (class_id) REFERENCES Classes (id) ON UPDATE CASCADE ON DELETE CASCADE
     )
@@ -261,41 +260,43 @@ def  update_lessons_table(connect, data:dict, delete: bool):
     connect.commit()
 
 
-#data = {date:list , day_id:int , lesson_id: int, class_id : int}
+#data = {date:list ,id:int, day:int , lesson_id: int, class_id : int}
 def update_schedule_table(connect, data:dict, delete: bool, ):
     date = data['date']
-    day_id = data['day_id']
+    id = data['id']
+    day = data['day']
     lesson_id = data['lesson_id']
     class_id = data['class_id']
+    lesson_time = data['lesson_time']
     query_insert = f'''
-    INSERT INTO Schedule_{date[0]}_{date[1]}_{date[2]}(day_id, lesson_id, class_id)
-    VALUES (?,?,?)
+    INSERT INTO Schedule_{date[0]}_{date[1]}_{date[2]}(day, lesson_id, class_id, lesson_time)
+    VALUES (?,?,?,?)
     '''
     query_delete = f'''
     DELETE FROM Schedule_{date[0]}_{date[1]}_{date[2]}
-    WHERE day_id = ?
+    WHERE id = ?
     '''
     # проверка входных данных на тип массива
-    if all(isinstance(arg, list) for arg in [day_id, lesson_id, class_id]):
-        values = [(i, j, k) for i, j, k in zip(day_id, lesson_id, class_id)]
+    if all(isinstance(arg, list) for arg in [day, lesson_id, class_id, lesson_time]):
+        values = [(i, j, k, l) for i, j, k, l in zip(day, lesson_id, class_id, lesson_time)]
         # вставка или удаление данных
         if delete:
-            Day = [(i,) for i in day_id]
+            Day = [(i,) for i in id]
             connect.cursor().executemany(query_delete, Day)
         else:
             connect.cursor().executemany(query_insert, values)
     # проверка входных данных на тип массива , (если 1 условие неверно , но верно 2 условие то вызывается ошибка несоответствия типов)
-    elif any(isinstance(arg, list) for arg in [day_id, lesson_id, class_id]):
+    elif any(isinstance(arg, list) for arg in [day, lesson_id, class_id, lesson_time]):
         raise TypeError
     # если элементы не имеют тип массива (строки, числа)
     else:
         # вставка или удаление данных
         if delete:
-            connect.cursor().execute(query_delete, (day_id,))
+            connect.cursor().execute(query_delete, (id,))
         else:
-            connect.cursor().execute(query_insert, (day_id, lesson_id, class_id))
+            connect.cursor().execute(query_insert, (day, lesson_id, class_id, lesson_time))
 
     connect.commit()
 
 if __name__ == '__main__':
-    create_schedule_table(sq.connect('2023.db'), ['2023', '3', '28'])
+    main()
--- TaskManager/scripts/server.py
+++ TaskManager/scripts/server.py
@@ -4,57 +4,100 @@ from DB import *
 
 app = Flask(__name__)
 
-#update функции таблиц базы данных
+# update функции таблиц базы данных
 update_functions = {
-    'update_teachers_table' : update_teachers_table,
-    'update_class_table' : update_class_table,
-    'update_lessons_table' : update_lessons_table,
-    'update_schedule_table' : update_schedule_table
+    'update_teachers_table': update_teachers_table,
+    'update_class_table': update_class_table,
+    'update_lessons_table': update_lessons_table,
+    'update_schedule_table': update_schedule_table
 
 }
 
-#Функция для получения данных приложения из базы данных
+
+def get_last_day(date):
+    months_days = {'01': '31', '02': '28', '03': '31', '04': '30', '05': '31',
+                   '06': '30', '07': '31', '08': '31', '09': '30', '10': '31',
+                   '11': '30', '12': '31'}
+
+    if int(date[0]) % 4 == 0:
+        months_days['02'] = '29'
+    if date[1] == '01':
+        last_month = '12'
+    else:
+        last_month = f'0{int(date[1][-1]) - 1}'
+    return months_days[last_month]
+
+
+def get_cards(data):
+    cards = list()
+    for lesson in data:
+        card = {'LessonName': lesson[1], 'TeacherName': lesson[2], 'ClassName': lesson[3], 'LessonTime': lesson[4]}
+        cards.append(card)
+
+    return cards
+
+
+# Функция для получения данных приложения из базы данных
 @app.route('/get_data', methods=['GET'])
 def get_data():
     try:
-        #получение данных из параметров get запроса
+        # получение данных из параметров get запроса
         date = request.args.getlist('date')
         table = request.args.get('table')
-        #подключение к базе данных
+        # подключение к базе данных
         connect = sq.connect(f'{date[0]}.db')
         cursor = connect.cursor()
-        #выборка данных изходя ищ параметров
+        # выборка данных изходя ищ параметров
         if table == 'Schedule':
-            cursor.execute(f'SELECT * FROM Schedule_{date[0]}_{date[1]}_{date[2]}')
+            schedule_table_name = f"Schedule_{date[0]}_{date[1]}_{date[2]}"
+            get_schedule_query = f"""
+            SELECT day, Lessons.name, Teachers.name, Classes.name, lesson_time
+            FROM {schedule_table_name}
+            INNER JOIN Lessons ON {schedule_table_name}.lesson_id = Lessons.id
+            INNER JOIN Teachers ON Lessons.teacher_id = Teachers.id
+            INNER JOIN Classes ON {schedule_table_name}.class_id = Classes.id
+            """
+            cursor.execute(get_schedule_query)
         else:
             cursor.execute(f'SELECT * FROM {table}')
         data = cursor.fetchall()
         connect.close()
-        #возвращение данных в json формате
-        return jsonify(data)
+        data_json = {'MonthInfo':
+                         {'LastMonthDays': get_last_day(date),
+                          'NowMonthDays': date[1],
+                          'SliceDay': data[0][0]
+                          },
+                     'HomeWork': {
+                         'Cards': get_cards(data)
+                     }
+                     }
+        # возвращение данных в json формате
+        return f"{data_json}"
 
     except Exception as e:
         return jsonify({'error': 'Bad Request', 'details': {'error': str(e)}})
 
-#функция для изменения данных базы данных приложением
+
+# функция для изменения данных базы данных приложением
 @app.route('/update_data', methods=['POST'])
 def update_data():
     try:
         data_request = request.get_json()
-        #записывание данных из json
+        # записывание данных из json
         year = data_request['year']
         function_update = data_request['function_update']
         data = data_request['data']
         delete = bool(data_request['delete'])
-        #подключение к базе данных
+        # подключение к базе данных
         connect = sq.connect(f'{year}.db')
-        #изменение данных в таблице
+        # изменение данных в таблице
         update_functions[function_update](connect, data, delete)
         connect.close()
         return 'Data updated successfully'
     except Exception as e:
-        return jsonify({'error': 'Bad Request', 'details' : {'error' : str(e)}})
+        return jsonify({'error': 'Bad Request', 'details': {'error': str(e)}})
+
 
 if __name__ == "__main__":
-    #запуск сервера
+    # запуск сервера
     app.run(debug=False, ssl_context=('../cert/certificate.crt', '../cert/privateKey.key'))
